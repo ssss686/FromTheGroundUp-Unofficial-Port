5c8a61580202367ff686aa2fdd5dfb9e4db86283
Fuxingcheng <niyaowoaxixi@163.com>
2026-08-11 17:05:12 +0800

    战利品重复修复

--- src/main/java/ftgumod/EventHandler.java
+++ src/main/java/ftgumod/EventHandler.java
@@ -1,8 +1,5 @@
 package ftgumod;
 
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import java.util.ArrayList;
@@ -21,9 +18,7 @@ import net.neoforged.neoforge.event.entity.living.MobEffectEvent;
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import org.apache.commons.lang3.tuple.Pair;
 
-import com.google.gson.JsonElement;
 import com.mojang.blaze3d.platform.InputConstants;
-import com.mojang.serialization.JsonOps;
 import ftgumod.api.util.BlockSerializable;
 import ftgumod.compat.jei.CompatJEI;
 import ftgumod.event.PlayerLockEvent;
@@ -37,23 +32,18 @@ import ftgumod.packet.server.RequestMessage;
 import ftgumod.technology.CapabilityTechnology;
 import ftgumod.technology.Technology;
 import ftgumod.technology.TechnologyManager;
-import ftgumod.util.LootUtils;
 import ftgumod.util.StackUtils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.util.GsonHelper;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ResultContainer;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.storage.loot.LootPool;
-import net.minecraft.world.level.storage.loot.LootTable;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -61,18 +51,13 @@ import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.InputEvent;
 import net.neoforged.neoforge.client.event.ScreenEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.LootTableLoadEvent;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerContainerEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
 
 public class EventHandler {
-
-	private static final Logger LOGGER = LogManager.getLogger();
 
 	private ItemStack stack = ItemStack.EMPTY;
 
@@ -236,31 +221,6 @@ public class EventHandler {
 					return;
 				}
 			}
-		}
-	}
-
-	@SubscribeEvent
-	public void onLootTableLoad(LootTableLoadEvent event) {
-		ResourceLocation name = event.getName();
-		if (name.getPath().equals("chests/village_blacksmith"))
-			injectLoot(event, ResourceLocation.fromNamespaceAndPath(FTGU.MODID, "inject/blacksmith"));
-		if (name.getPath().equals("chests/desert_pyramid"))
-			injectLoot(event, ResourceLocation.fromNamespaceAndPath(FTGU.MODID, "inject/pyramid"));
-		if (name.getPath().equals("chests/stronghold_library"))
-			injectLoot(event, ResourceLocation.fromNamespaceAndPath(FTGU.MODID, "inject/library"));
-	}
-
-	private void injectLoot(LootTableLoadEvent event, ResourceLocation injectLoc) {
-		try {
-			String path = "/assets/" + injectLoc.getNamespace() + "/loot_tables/" + injectLoc.getPath() + ".json";
-			Reader reader = new InputStreamReader(getClass().getResourceAsStream(path), StandardCharsets.UTF_8);
-			JsonElement json = GsonHelper.parse(reader);
-			LootTable injectTable = LootTable.DIRECT_CODEC.parse(JsonOps.INSTANCE, json)
-					.resultOrPartial(LOGGER::error).orElse(null);
-			if (injectTable != null)
-				event.setTable(LootUtils.merge(event.getTable(), injectTable));
-		} catch (Exception e) {
-			LOGGER.error("Failed to load inject loot table: {}", injectLoc, e);
 		}
 	}
 
