e143fb8546accf29cdd8136af0030bb636532665
Fuxingcheng <niyaowoaxixi@163.com>
2026-08-24 12:45:02 +0800

    Initial commit: Fabric mod files

---
+++ src/main/java/com/fuxingcheng/fromthegroundup/FromTheGroundUpClient.java
@@ -0,0 +1,10 @@
+package com.fuxingcheng.fromthegroundup;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FromTheGroundUpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
